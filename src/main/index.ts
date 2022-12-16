a8be037ea9f4ce610653b39686b3c28c4d2e18e3
965217357@qq.com <965217357@qq.com>
2022-12-16 17:47:47 +0800

    修改配置（允许分号结尾）

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,6 @@
-import { app, shell, BrowserWindow } from 'electron'
-import * as path from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import { app, shell, BrowserWindow } from 'electron';
+import * as path from 'path';
+import { is } from '@electron-toolkit/utils';
 
 function createWindow(): void {
   // Create the browser window.
@@ -18,57 +18,43 @@ function createWindow(): void {
       preload: path.join(__dirname, '../preload/index.js'),
       sandbox: false
     }
-  })
+  });
 
   mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+    mainWindow.show();
+  });
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
+    shell.openExternal(details.url);
+    console.log('details.url', details.url);
+    return { action: 'deny' };
+  });
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
+  // 热更新(HMR)判断加载url还是本地文件
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  // electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
+  createWindow();
 
-  createWindow()
-
+  // macOS特殊处理，启动时没有窗口则创建一个
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
+});
--- src/renderer/src/renderer.ts
+++ src/renderer/src/renderer.ts
@@ -1,22 +1,22 @@
 export function init(): void {
   window.addEventListener('DOMContentLoaded', () => {
-    doAThing()
-  })
+    doAThing();
+  });
 }
 
 function doAThing(): void {
-  const versions = window.electron.process.versions
-  replaceText('.electron-version', `Electron v${versions.electron}`)
-  replaceText('.chrome-version', `Chromium v${versions.chrome}`)
-  replaceText('.node-version', `Node v${versions.node}`)
-  replaceText('.v8-version', `V8 v${versions.v8}`)
+  const versions = window.electron.process.versions;
+  replaceText('.electron-version', `Electron v${versions.electron}`);
+  replaceText('.chrome-version', `Chromium v${versions.chrome}`);
+  replaceText('.node-version', `Node v${versions.node}`);
+  replaceText('.v8-version', `V8 v${versions.v8}`);
 }
 
 function replaceText(selector: string, text: string): void {
-  const element = document.querySelector<HTMLElement>(selector)
+  const element = document.querySelector<HTMLElement>(selector);
   if (element) {
-    element.innerText = text
+    element.innerText = text;
   }
 }
 
-init()
+init();
